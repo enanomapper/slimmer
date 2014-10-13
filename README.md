5888f42969508874d1b825d0acc87b26c740a45a
Egon Willighagen <egon.willighagen@gmail.com>
2014-10-13 19:38:35 +0200

    It can now read an ontology

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 Compile
 -------
 
-    $ mvn compiler:compile
+    $ mvn install
+
+Run
+---
+
+    $ java -cp target/slimmer-0.0.1-SNAPSHOT.jar com.github.enanomapper.Slimmer src/main/resources
--- src/main/java/com/github/enanomapper/Slimmer.java
+++ src/main/java/com/github/enanomapper/Slimmer.java
@@ -1,9 +1,48 @@
 package com.github.enanomapper;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+import java.util.Properties;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.profiles.OWL2DLProfile;
+import org.semanticweb.owlapi.profiles.OWLProfileReport;
+import org.semanticweb.owlapi.profiles.OWLProfileViolation;
+import org.semanticweb.owlapi.util.SimpleIRIMapper;
+
 public class Slimmer {
 
 	public static void main(String[] args) {
-		
+		String rootFolder = args[0];
+		System.out.println("Searching configuration files in " + rootFolder);
+		File dir = new File(rootFolder);
+		File[] files = dir.listFiles(new FilenameFilter() {
+		    public boolean accept(File dir, String name) {
+		        return name.toLowerCase().endsWith(".props");
+		    }
+		});
+		for (File file : files) {
+			try {
+				System.out.println("Slimming for " + file.getName());
+				Properties props = new Properties();
+				props.load(new FileReader(file));
+				String owlFilename = props.getProperty("owl");
+				File owlFile = new File(owlFilename);
+				OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+				OWLOntology onto = man.loadOntology(
+					IRI.create("file://" + owlFile.getAbsoluteFile())
+				);
+				System.out.println("Loaded axioms: " + onto.getAxiomCount());
+			} catch (Exception e) {
+				System.err.println("Cannot read file: " + file.getAbsoluteFile());
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
