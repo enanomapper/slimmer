694d38a41d765ae8c3af7fba80a015b1a238bf8d
Egon Willighagen <egon.willighagen@gmail.com>
2014-10-28 08:15:56 +0100

    Added tests for the slimming

---
+++ src/test/java/com/github/enanomapper/SlimmerTest.java
@@ -0,0 +1,70 @@
+package com.github.enanomapper;
+
+import java.io.InputStream;
+import java.io.StringReader;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+
+public class SlimmerTest {
+
+	@Test
+	public void testLoading() throws OWLOntologyCreationException {
+		InputStream stream = this.getClass().getClassLoader().getResourceAsStream("bfo-1.1.owl");
+		Slimmer slimmer = new Slimmer(stream);
+		OWLOntology ontology = slimmer.getOntology();
+		Assert.assertNotNull(ontology);
+		Assert.assertNotSame(0, ontology.getAxiomCount());
+	}
+
+	@Test
+	public void testParsingUp() throws Exception {
+		String test = "+U:http://www.ifomis.org/bfo/1.1/snap#DependentContinuant";
+		Configuration conf = new Configuration();
+		conf.read(new StringReader(test));
+		Set<Instruction> irisToSave = conf.getTreePartsToSave();
+		
+		Assert.assertEquals(1, conf.getTreePartsToSave().size());
+		InputStream stream = this.getClass().getClassLoader().getResourceAsStream("bfo-1.1.owl");
+		Slimmer slimmer = new Slimmer(stream);
+		slimmer.removeAllExcept(irisToSave);
+		OWLOntology ontology = slimmer.getOntology();
+		Assert.assertNotNull(ontology);
+		Assert.assertNotSame(3, ontology.getClassesInSignature().size());
+	}
+
+	@Test
+	public void testParsingSingle() throws Exception {
+		String test = "+:http://www.ifomis.org/bfo/1.1/snap#DependentContinuant";
+		Configuration conf = new Configuration();
+		conf.read(new StringReader(test));
+		Set<Instruction> irisToSave = conf.getTreePartsToSave();
+		
+		Assert.assertEquals(1, conf.getTreePartsToSave().size());
+		InputStream stream = this.getClass().getClassLoader().getResourceAsStream("bfo-1.1.owl");
+		Slimmer slimmer = new Slimmer(stream);
+		slimmer.removeAllExcept(irisToSave);
+		OWLOntology ontology = slimmer.getOntology();
+		Assert.assertNotNull(ontology);
+		Assert.assertNotSame(1, ontology.getClassesInSignature().size());
+	}
+
+	@Test
+	public void testParsingDown() throws Exception {
+		String test = "+D:http://www.ifomis.org/bfo/1.1/snap#MaterialEntity";
+		Configuration conf = new Configuration();
+		conf.read(new StringReader(test));
+		Set<Instruction> irisToSave = conf.getTreePartsToSave();
+		
+		Assert.assertEquals(1, conf.getTreePartsToSave().size());
+		InputStream stream = this.getClass().getClassLoader().getResourceAsStream("bfo-1.1.owl");
+		Slimmer slimmer = new Slimmer(stream);
+		slimmer.removeAllExcept(irisToSave);
+		OWLOntology ontology = slimmer.getOntology();
+		Assert.assertNotNull(ontology);
+		Assert.assertNotSame(4, ontology.getClassesInSignature().size());
+	}
+}
