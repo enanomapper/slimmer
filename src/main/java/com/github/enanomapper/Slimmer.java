097fd81cbd1d7c78797d401ba53dc90c9774beae
Egon Willighagen <egon.willighagen@gmail.com>
2014-10-29 16:06:32 +0100

    Get some debug info on the ontology annotations. Are those the dc:source etc?

--- src/main/java/com/github/enanomapper/Slimmer.java
+++ src/main/java/com/github/enanomapper/Slimmer.java
@@ -1,6 +1,8 @@
 package com.github.enanomapper;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FilenameFilter;
 import java.io.InputStream;
@@ -35,28 +37,34 @@ public class Slimmer {
 	private OWLOntologyManager man;
 	private OWLOntology onto;
 
-	public Slimmer(File owlFile, String mergedOntologyIRI) throws OWLOntologyCreationException {
-		man = OWLManager.createOWLOntologyManager();
-		onto = man.loadOntology(
-			IRI.create("file://" + owlFile.getAbsoluteFile())
-		);
-		Set<OWLImportsDeclaration> importDeclarations = onto.getImportsDeclarations();
-		for (OWLImportsDeclaration declaration : importDeclarations) {
-			try {
-				man.loadOntology(declaration.getIRI());
-				System.out.println("Loaded imported ontology: " + declaration.getIRI());
-			} catch (Exception exception) {
-				System.out.println("Failed to load imported ontology: " + declaration.getIRI());
-			}
-		}
-		// Merge all of the loaded ontologies, specifying an IRI for the new ontology
-		OWLOntologyMerger merger = new OWLOntologyMerger(man);
-		onto = merger.createMergedOntology(man, IRI.create(mergedOntologyIRI));
+	public Slimmer(File owlFile, String mergedOntologyIRI) throws OWLOntologyCreationException, FileNotFoundException {
+		this(new FileInputStream(owlFile), mergedOntologyIRI);
 	}
 
 	public Slimmer(InputStream owlFile) throws OWLOntologyCreationException {
+		this(owlFile, null);
+	}
+
+	public Slimmer(InputStream owlFile, String mergedOntologyIRI) throws OWLOntologyCreationException {
 		man = OWLManager.createOWLOntologyManager();
 		onto = man.loadOntologyFromOntologyDocument(owlFile);
+		if (mergedOntologyIRI != null) {
+			Set<OWLImportsDeclaration> importDeclarations = onto.getImportsDeclarations();
+			for (OWLImportsDeclaration declaration : importDeclarations) {
+				try {
+					man.loadOntology(declaration.getIRI());
+					System.out.println("Loaded imported ontology: " + declaration.getIRI());
+				} catch (Exception exception) {
+					System.out.println("Failed to load imported ontology: " + declaration.getIRI());
+				}
+			}
+			// Merge all of the loaded ontologies, specifying an IRI for the new ontology
+			OWLOntologyMerger merger = new OWLOntologyMerger(man);
+			onto = merger.createMergedOntology(man, IRI.create(mergedOntologyIRI));
+			for (OWLOntology ontology : man.getOntologies()) {
+				System.out.println("  copying axioms: " + ontology.getAnnotations().toArray()[0]);
+			}
+		}
 	}
 
 	public OWLOntology getOntology() {
