572ad9e02ab5f45d325c7053e921d27b82a4e5f3
Egon Willighagen <egon.willighagen@gmail.com>
2015-11-23 08:25:16 +0100

    Testing the combination of 'select down' with a new super class

--- src/test/java/com/github/enanomapper/SlimmerTest.java
+++ src/test/java/com/github/enanomapper/SlimmerTest.java
@@ -185,6 +185,35 @@ public class SlimmerTest {
 	}
 
 	@Test
+	public void testParsingWithNewSuperClass() throws Exception {
+		String test = "+D(http://www.ifomis.org/bfo/1.1/snap#Entity):http://www.ifomis.org/bfo/1.1/snap#Object";
+		Configuration conf = new Configuration();
+		conf.read(new StringReader(test));
+		Set<Instruction> irisToSave = conf.getTreePartsToSave();
+		Iterator<Instruction> instructions = irisToSave.iterator();
+		Instruction instruction = instructions.next();
+		if (!instruction.getUriString().endsWith("Object")) instruction = instructions.next();
+		String baseClass = instruction.getUriString();
+
+		Assert.assertEquals(1, conf.getTreePartsToSave().size());
+		InputStream stream = this.getClass().getClassLoader().getResourceAsStream("bfo-1.1.owl");
+		Slimmer slimmer = new Slimmer(stream);
+		slimmer.removeAllExcept(irisToSave);
+		OWLOntology ontology = slimmer.getOntology();
+		Assert.assertNotNull(ontology);
+		Assert.assertEquals(2, ontology.getClassesInSignature().size());
+
+		Set<OWLEntity> entities = ontology.getEntitiesInSignature(IRI.create(baseClass));
+		Assert.assertEquals(1, entities.size());
+		OWLEntity entity = entities.iterator().next();
+		Assert.assertTrue(entity.isOWLClass());
+		OWLClass owlClass = entity.asOWLClass();
+		Set<OWLClassAxiom> axioms = ontology.getAxioms(owlClass, Imports.INCLUDED);
+		Assert.assertEquals(1, axioms.size());
+		Assert.assertEquals("SubClassOf", axioms.iterator().next().getAxiomType().getName());
+	}
+
+	@Test
 	public void testMakeNewSubclassProperty() throws Exception {
 		String test = "+:http://www.ifomis.org/bfo/1.1#Entity\n"
 	                + "+(http://www.ifomis.org/bfo/1.1#Entity):http://www.ifomis.org/bfo/1.1/snap#MaterialEntity";
